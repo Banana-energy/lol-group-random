33c389713bffd0202d4b2744c4cbf7df64dc5f85
杨柠滔 <372728339@qq.com>
2023-07-18 09:51:47 +0800

    ci: 同步修改tauri.conf.json中的版本号

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -7,6 +7,7 @@ const require = createRequire(import.meta.url);
 async function release() {
   const flag = process.argv[2] ?? 'patch';
   const packageJson = require('../package.json');
+  const tauriConfig = require('../src-tauri/tauri.conf.json');
   let [a, b, c] = packageJson.version.split('.').map(Number);
 
   if (flag === 'major') {
@@ -25,10 +26,15 @@ async function release() {
 
   const nextVersion = `${a}.${b}.${c}`;
   packageJson.version = nextVersion;
+  tauriConfig.package.version = nextVersion;
 
   fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+  fs.writeFileSync(
+    './src-tauri/tauri.conf.json',
+    JSON.stringify(tauriConfig, null, 2)
+  );
 
-  execSync('git add ./package.json');
+  execSync('git add ./package.json ./src-tauri/tauri.conf.json');
   execSync(`git commit -m "build: v${nextVersion}"`);
   execSync(`git tag -a v${nextVersion} -m "v${nextVersion}"`);
   execSync(`git push`);
